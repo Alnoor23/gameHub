e25ad0180c4aefb2fbc3846be7001bb983347589
Alnoor <alnoorjaria23@gmail.com>
2023-10-05 22:04:07 +0100

    Fetch the Genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,14 @@
+import useGenres from "../hooks/useGenres";
+
+const GenreList = () => {
+  const { genres } = useGenres();
+  return (
+    <ul>
+      {genres.map((e) => (
+        <li key={e.id}>{e.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreList;
