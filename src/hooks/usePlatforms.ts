53ce77262704164fb6efe03c2f02a77d3cc9577c
Alnoor <alnoorjaria23@gmail.com>
2023-10-12 00:02:23 +0100

    fic build errors

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,9 +1,9 @@
 import platforms from "../data/platforms";
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+// interface Platform {
+//   id: number;
+//   name: string;
+//   slug: string;
+// }
 
 // const usePlatform = () => useData<Platform>("/platforms/lists/parents");
 const usePlatform = () => ({ data: platforms, isLoading: false, error: null });
